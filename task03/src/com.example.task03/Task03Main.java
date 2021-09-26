da94d5d5e5b5a9cfc4d64db515f7d9ec021fe15e
MagTemTenebrius <magtemtenebrius@gmail.com>
2021-09-26 15:51:14 +0500

    task03 successful

--- task03/src/com.example.task03/Task03Main.java
+++ task03/src/com.example.task03/Task03Main.java
@@ -10,6 +10,9 @@ public class Task03Main {
     }
 
     static String getNameOfWeekDays(int weekDaysNumber) {
-        return "";//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if(weekDaysNumber < 1 || weekDaysNumber > 7)
+            return "такого дня недели не существует";
+        String[] days = {"понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресение"};
+        return days[weekDaysNumber-1];//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
     }
 }
